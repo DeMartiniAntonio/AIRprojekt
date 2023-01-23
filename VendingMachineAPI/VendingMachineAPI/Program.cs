7c0711cc48ec1c0b527f1e0b8525e6d19eaf2394
DESKTOP-CCAHGBT\Veronika <vtvrdy@foi.hr>
2023-01-23 12:17:35 +0100

    Promjena connection stringa

--- VendingMachineAPI/VendingMachineAPI/Program.cs
+++ VendingMachineAPI/VendingMachineAPI/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using VendingMachineAPI.Data;
 using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.Extensions.Configuration;
+using FluentAssertions.Common;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +16,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //builder.Services.AddDbContext<VendingMachineAPIDbContext>(options => options.UseInMemoryDatabase("VendingMachineDb"));
-builder.Services.AddDbContext<VendingMachineAPIDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("VendingMachineApp"), ServerVersion.AutoDetect("8.0.22")));
+//builder.Services.AddDbContext<VendingMachineAPIDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("VendingMachineApp"), ServerVersion.AutoDetect("8.0.22")));
+//builder.Services.AddDbContext<VendingMachineAPIDbContext>(options => options.UseMySql(configuration.GetConnectionString("vendingmachinedb")));
+builder.Services.AddDbContext<VendingMachineAPIDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("vendingmachinedb"), ServerVersion.AutoDetect("8.0.22")));
 
 
 var app = builder.Build();
