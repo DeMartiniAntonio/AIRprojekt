60a071edac97f04e977643affb1c2f637c817af5
alpezajosip <59579075+alpezajosip@users.noreply.github.com>
2023-02-08 11:08:23 +0100

    Inicijalni ekran za pregled eventova

--- client/src/pages/events.jsx
+++ client/src/pages/events.jsx
@@ -1,5 +1,55 @@
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+
+import { api } from "../utils/api";
+
+const columns = [
+  {
+    title: "Event ID",
+    dataIndex: "event_ID",
+    key: "eventId",
+  },
+  {
+    title: "Date",
+    key: "date",
+    render: (record) => {
+      const date = new Date(record.date_time);
+      return date.toLocaleString("en-GB");
+    },
+  },
+  {
+    title: "Device ID",
+    dataIndex: "device_id",
+    key: "deviceId",
+  },
+  {
+    title: "User ID",
+    dataIndex: "user_id",
+    key: "userId",
+  },
+];
+
 const Events = () => {
-  return <div>Events page</div>;
+  const [events, setEvents] = useState();
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const fetchEvents = () => {
+    fetch(`${api}/GetAll?ObjectsType=events`)
+      .then((res) => res.json())
+      .then((res) => {
+        setEvents(res);
+        console.log(res);
+      });
+  };
+
+  return (
+    <>
+      <Table columns={columns} dataSource={events} />
+    </>
+  );
 };
 
 export default Events;
