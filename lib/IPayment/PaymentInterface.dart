0da57fccbcf3e6e29c8fd094c2362f891a40985c
Sara S <lovro.posaric@gmail.com>
2023-09-01 14:44:18 +0200

    dodavanje Widget getPayButton unutar interface-a

--- lib/IPayment/PaymentInterface.dart
+++ lib/IPayment/PaymentInterface.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 
 abstract class PaymentInterface {
   void executePayment(BuildContext context, PaymentListener listener, String amount);
+  Widget getPayButton(BuildContext context);
 }
 
 abstract class PaymentListener{
