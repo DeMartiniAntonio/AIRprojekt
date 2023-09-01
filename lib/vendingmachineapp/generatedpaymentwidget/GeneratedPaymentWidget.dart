c2d1f608a9f557482472e09db6e096344b9ffbcb
Sara S <lovro.posaric@gmail.com>
2023-09-01 14:48:26 +0200

    izmjena GeneratedPaymentWidget radi poboljšanja i uključivanja modularnosti

--- lib/vendingmachineapp/generatedpaymentwidget/GeneratedPaymentWidget.dart
+++ lib/vendingmachineapp/generatedpaymentwidget/GeneratedPaymentWidget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/vendingmachineapp/generatedpaymentwidget/generated/Button_BraintreeWidget.dart';
-import 'package:flutterapp/vendingmachineapp/generatedpaymentwidget/generated/Button_OtherWidget.dart';
-import 'package:flutterapp/vendingmachineapp/generatedpaymentwidget/generated/Button_RevolutWidget.dart';
-import 'package:flutterapp/vendingmachineapp/generatedpaymentwidget/generated/GeneratedLabel_PaymentWidget.dart';
+import 'package:flutterapp/Braintree/BraintreePay.dart';
+import 'package:flutterapp/RevolutPayment/RevolutPay.dart';
+import 'package:flutterapp/StripePayment/StripePay.dart';
+import 'package:flutterapp/ThirdModule/ThirdModule.dart';
 import 'package:flutterapp/helpers/transform/transform.dart';
-import 'package:flutterapp/vendingmachineapp/generatedpaymentwidget/generated/Button_PayNow.dart';
+
+import '../../IPayment/PaymentInterface.dart';
 
 
 
@@ -14,116 +15,33 @@ import 'package:flutterapp/vendingmachineapp/generatedpaymentwidget/generated/Bu
 class GeneratedPaymentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    var paymentInterfaceList = <PaymentInterface>[
+      RevolutPay(),
+      BraintreePay(),
+      StripePay(),
+      ThirdModule()
+    ];
+
+    var paymentButtonList = <Widget>[];
+
+    for (var paymentInterface in paymentInterfaceList) {
+      paymentButtonList.add(
+        Container(
+          margin: EdgeInsets.only(top: 25),
+          child: paymentInterface.getPayButton(context),
+        )
+      );
+    }
+
     return Material(
-
-        child: ClipRRect(
-      borderRadius: BorderRadius.zero,
-          child: Container(
-        width: 390.0,
-        height: 844.0,
-            child: Stack(
-            fit: StackFit.expand,
-            alignment: Alignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.zero,
-                child: Container(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
-              ),
-              Positioned(
-                left: 0,
-                top: 0,
-                right: 0.0,
-                bottom: null,
-                width: null,
-                height: 85.0,
-                child: GeneratedLabel_PaymentWidget(),
-              ),
-              Positioned(
-                left: 39.0,
-                top: null,
-                right: 39.0,
-                bottom: null,
-                width: null,
-                height: 62.04999923706055,
-                child: TransformHelper.translate(
-                    x: 0.00,
-                    y: -35.98,
-                    z: 0,
-                    child: Button_OtherWidget()),
-              ),
-              Positioned(
-                left: 39.0,
-                top: null,
-                right: 39.0,
-                bottom: null,
-                width: null,
-                height: 62.04999923706055,
-                child: TransformHelper.translate(
-                    x: 0.00,
-                    y: 64.02,
-                    z: 0,
-                    child: Button_PayNow()),
-              ),
-
-              Positioned(
-                left: 39.0,
-                top: null,
-                right: 39.0,
-                bottom: null,
-                width: null,
-                height: 62.04999923706055,
-                child: TransformHelper.translate(
-                    x: 0.00,
-                    y: -135.98,
-                    z: 0,
-                    child: Button_RevolutWidget()),
-              ),
-
-              Positioned(
-                left: 39.0,
-                top: null,
-                right: 39.0,
-                bottom: null,
-                width: null,
-                height: 62.04999923706055,
-                child: TransformHelper.translate(
-                  x: 0.0,
-                  y: 160,
-                  z: 0,
-                  child: Button_BraintreeWidget(),
-                ),
-              ),
-
-              Positioned(
-                left: null,
-                top: null,
-                right: null,
-                bottom: null,
-                width: 213.64999389648438,
-                height: 50.45000076293945,
-                child: TransformHelper.translate(
-                    x: -0.18,
-                    y: -247.77,
-                    z: 0,
-                    child: Text(
-                      '''Choose a payment method''',
-                      overflow: TextOverflow.visible,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        height: 1.210227219954781,
-                        fontSize: 23.0,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w700,
-                        color: Color.fromARGB(255, 0, 0, 0),
-
-                        /* letterSpacing: 0.0, */
-                      ),
-                    )),
-              )
-            ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Choose a payment method", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          ...paymentButtonList
+        ],
       ),
-    ));
+    );
   }
 }
